8f8915a5e996b147e97da756809857bc08aa6b9f
damirian174 <damir_ahmedzyanov@mail.ru>
2024-09-22 21:34:01 +0300

    create models

--- src/database/models/models.py
+++ src/database/models/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
 from base import Base
 
 # Создание модели роль
@@ -10,3 +10,40 @@ class Role(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
 
+
+class Course(Base):
+    __tablename__ = "course"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(600), nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
+
+
+class Task(Base):
+    __tablename__ = "task"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    diff: Mapped[str] = mapped_column(String(500), nullable=False)
+    description: Mapped[str] = mapped_column(String(1000), nullable=False)
+    solution: Mapped[str] = mapped_column(String(1000), nullable=True)
+
+class User(Base):
+    __tablename__ = "user"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    role_id: Mapped[int] = mapped_column(Integer, ForeignKey('role.id'))
+    course_id: Mapped[int] = mapped_column(Integer, ForeignKey('course.id'))
+    login: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(50), nullable=False)
+
+    role: Mapped[Role] = relationship("Role", back_populates="users")
+    course: Mapped[Course] = relationship("Course", back_populates="users")
+
+class CourseTask(Base):
+    __tablename__ = "course_task"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    course_id: Mapped[int] = mapped_column(Integer, ForeignKey('course.id'))
+    task_id: Mapped[int] = mapped_column(Integer, ForeignKey('task.id'))
+
+    course: Mapped[Course] = relationship("Course", back_populates="coursetasks")
+    task: Mapped[Task] = relationship("Task", back_populates="coursetasks")
