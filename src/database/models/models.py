03d584aa5d8569f6bdd25fb9d32419b3bfa13ce8
damirian174 <damir_ahmedzyanov@mail.ru>
2024-09-30 20:21:08 +0300

    correct models and sessions after tests

--- src/database/models/models.py
+++ src/database/models/models.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
 from src.database.models.base import Base
-# Создание модели роль
-
 
 class Role(Base):
     __tablename__ = 'role'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(50), nullable=False)
 
+    # Определяем отношение к пользователям
+    users = relationship("User", back_populates="role")
 
 class Course(Base):
     __tablename__ = "course"
@@ -17,15 +17,23 @@ class Course(Base):
     description: Mapped[str] = mapped_column(String(600), nullable=False)
     price: Mapped[float] = mapped_column(Numeric(10, 2), nullable=False)
 
+    user = relationship("User", back_populates="courses")
+    coursetasks = relationship("CourseTask", back_populates="course")
+    tasks = relationship("Task", back_populates="course")
 
 class Task(Base):
     __tablename__ = "task"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    course_id: Mapped[int] = mapped_column(Integer, ForeignKey('course.id'))  # Добавлено для указания внешнего ключа
     name: Mapped[str] = mapped_column(String(100), nullable=False)
     diff: Mapped[str] = mapped_column(String(500), nullable=False)
     description: Mapped[str] = mapped_column(String(1000), nullable=False)
     solution: Mapped[str] = mapped_column(String(1000), nullable=True)
 
+    # Определение отношения к курсу
+
+    course = relationship("Course", back_populates="tasks")
+    coursetasks = relationship("CourseTask", back_populates="task")  # Добавлено для обратного отношения
 
 class User(Base):
     __tablename__ = "user"
@@ -36,14 +44,14 @@ class User(Base):
     login: Mapped[str] = mapped_column(String(50), nullable=False, unique=True)
     password: Mapped[str] = mapped_column(String(50), nullable=False)
 
-    role: Mapped[Role] = relationship("Role", back_populates="users")
-    course: Mapped[Course] = relationship("Course", back_populates="users")
-
+    role = relationship("Role", back_populates="users")
+    courses = relationship("Course", back_populates="user")
 
 class CourseTask(Base):
     __tablename__ = "course_task"
     course_id: Mapped[int] = mapped_column(Integer, ForeignKey('course.id'), primary_key=True)
-    task_id: Mapped[int] = mapped_column(Integer, ForeignKey('task.id'),  primary_key=True)
+    task_id: Mapped[int] = mapped_column(Integer, ForeignKey('task.id'), primary_key=True)
 
-    course: Mapped[Course] = relationship("Course", back_populates="coursetasks")
+    # Отношение к курсу
+    course = relationship("Course", back_populates="coursetasks")
     task: Mapped[Task] = relationship("Task", back_populates="coursetasks")
--- src/database/models/repo.py
+++ src/database/models/repo.py
@@ -10,37 +10,37 @@ class LearningRepo:
     def __init__(self, session: AsyncSession, orm_model):
         self.session = session
         self.orm_model = orm_model
-# Получить все записи
+
+    # Получить все записи
     async def get_all(self):
-        async with self.session() as session:
-            result = await session.execute(select(self.orm_model))
-            return result.scalars().all()
-# Получить записи по id
+        result = await self.session.execute(select(self.orm_model))
+        return result.scalars().all()
+
+    # Получить записи по id
     async def get_one_by_id(self, id: int):
-        async with self.session() as session:
-            result = await session.execute(select(self.orm_model).where(self.orm_model.id == id))
-            return result.scalar_one_or_none()
-# Добавить запись
+        result = await self.session.execute(select(self.orm_model).where(self.orm_model.id == id))
+        return result.scalar_one_or_none()
+
+    # Добавить запись
     async def add_one(self, data):
-        async with self.session() as session:
-            new_record = self.orm_model(**data)
-            session.add(new_record)
-            await session.commit()
-            return new_record
-# Удалить запись
+        new_record = self.orm_model(**data)
+        self.session.add(new_record)
+        await self.session.commit()
+        return new_record
+
+    # Удалить запись
     async def delete_one(self, id: int):
-        async with self.session() as session:
-            record = await self.get_one_by_id(id)
-            if record:
-                await session.delete(record)
-                await session.commit()
-# Изменить запись
+        record = await self.get_one_by_id(id)
+        if record:
+            await self.session.delete(record)
+            await self.session.commit()
+
+    # Изменить запись
     async def change_data_by_id(self, id: int, new_data):
-        async with self.session() as session:
-            record = await self.get_one_by_id(id)
-            if record:
-                for key, value in new_data.items():
-                    setattr(record, key, value)
-                await session.commit()
-                return record
-            return None
+        record = await self.get_one_by_id(id)
+        if record:
+            for key, value in new_data.items():
+                setattr(record, key, value)
+            await self.session.commit()
+            return record
+        return None
